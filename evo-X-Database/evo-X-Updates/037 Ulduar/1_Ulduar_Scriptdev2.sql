8a33857de67c3b32c01769766c2f9e251fadd253
Salja <beleko2009@web.de>
2010-03-08 16:36:35 +0100

    [40] Fix Ulduar SQL
    Signed-off-by: Salja <beleko2009@web.de>

--- evo-X-Database/evo-X-Updates/037 Ulduar/1_Ulduar_Scriptdev2.sql
+++ evo-X-Database/evo-X-Updates/037 Ulduar/1_Ulduar_Scriptdev2.sql
@@ -8,4 +8,4 @@ INSERT INTO script_texts VALUES
 (-1603005, "I'm ready to play!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 15726, 1, 0, 0, "XT-002 Heart Closed"),
 (-1603006, "Time for a new game! My old toys will fight my new toys!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 15732, 1, 0, 0, "XT-002 Adds"),
 (-1603007, "I'm tired of these toys. I don't want to play anymore!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 15730, 1, 0, 0, "XT-002 Berserk"),
-(-1603008, "You are bad... Toys... Very... Baaaaad!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 15731, 1, 0, 0, "XT-002 Death"),
+(-1603008, "You are bad... Toys... Very... Baaaaad!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 15731, 1, 0, 0, "XT-002 Death");
